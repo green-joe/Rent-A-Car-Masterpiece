5260f74e60fd2851470d950ddb041ee5a90ee57e
sz-jozsef <sz.jozsef.developer@gmail.com>
2023-02-23 13:17:07 +0100

    registration page is begun

--- rentacarfrontend/src/pages/RegistrationPage.jsx
+++ rentacarfrontend/src/pages/RegistrationPage.jsx
@@ -1,7 +1,6 @@
-
 import React, { useState } from 'react';
 import Form from "react-bootstrap/Form";
-import { Col, Container, FormGroup, Row, Card, Button, FormControl } from 'react-bootstrap';
+import { Col, FormGroup, Row, Card, Button, FormControl } from 'react-bootstrap';
 import FormLabel from 'react-bootstrap/esm/FormLabel';
 
 const RegistrationPage = () => {
@@ -18,17 +17,34 @@ const RegistrationPage = () => {
   return (
     
       <Row className="p-4 d-flex justify-content-center align-items-center">
-        <Col md={8} lg={6} xs={12}>
-         
+        <Col md={8} lg={6} xs={12}>         
           <Card className="shadow">
           <div className="square border border-3 border-primary">
-            <Card.Body>
-               
+            <Card.Body>               
               <div className='mb-3 mt-md-4'>
                 <h1 className="fw-bold mb-2 text-center ">Registration</h1>
                 <p className='mb-3 text-center'>Please add your firstname,lastname,email and password!</p>
                 <div className='mb-3'>
                   <Form onSubmit={handleSubmit} className="form">
+                  <FormGroup controlId='firstname' className="mb-3">
+                      <FormLabel className='text-center' >First name:</FormLabel>
+                      <FormControl
+                        type="text"
+                        value={firstName}
+                        placeholder="First name"
+                        onChange={(event) => setFirstName(event.target.value)}
+                      />
+                    </FormGroup>
+                    <FormGroup controlId='lastname' className="mb-3">
+                      <FormLabel>Last name:</FormLabel>
+                      <FormControl
+                        type="Last name"
+                        placeholder=""
+                        value={lastName}
+                        onChange={(event) => setLastName(event.target.value)}
+                      />
+                      </FormGroup>
+
                     <FormGroup controlId='email' className="mb-3">
                       <FormLabel className='text-center' >Email:</FormLabel>
                       <FormControl
@@ -47,39 +63,18 @@ const RegistrationPage = () => {
                         onChange={(event) => setPassword(event.target.value)}
                       />
 
-                    </FormGroup>
-                    <FormGroup className="mb-3"controlId="checkBox">
-                      <p clascName="small">
-                        <a className="text-primary" href="#">
-                          Forgot password?
-                        </a>
-                      </p>                      
-                    </FormGroup>
+                    </FormGroup>                    
                     <div className="d-grid">
-                    <Button variant="primary" type="submit">Login</Button>
+                    <Button variant="primary" type="submit">Register</Button>
                     </div>
-                  </Form>
-                  <div className="mt-3">
-                    <p className="mb-0 text-center">
-                      Don't have an acconunt?{""}
-                      <a href="{''}" className="text.primary fw-bold">
-                        Sign up
-                      </a>
-                    </p>
+                  </Form>                 
                   </div>
-                  </div>
-                </div>
-             
+                </div>             
             </Card.Body> </div>
           </Card>
         </Col>
       </Row>
-    
-
-
   );
 };
-
-
 
 export default RegistrationPage;
