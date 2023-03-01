8d684ff0b33cfe9f2e9336d6db7cc2322d800edc
sz-jozsef <sz.jozsef.developer@gmail.com>
2023-03-01 20:37:45 +0100

    register form popup has done

--- rentacarfrontend/src/pages/RegistrationPage.jsx
+++ rentacarfrontend/src/pages/RegistrationPage.jsx
@@ -2,64 +2,77 @@ import React, { useState } from 'react';
 import Form from "react-bootstrap/Form";
 import { Col, FormGroup, Row, Card, Button, FormControl } from 'react-bootstrap';
 import FormLabel from 'react-bootstrap/esm/FormLabel';
+import Popup from '../services/Popup'
+import '../styles/popup.css'
+
+
 
 const RegistrationPage = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-  const [firstName, setFirstName]=useState('')
-  const [lastName, setLastName]=useState('')
-  const [message, setMessage]=useState('')
+  const [firstName, setFirstName] = useState('')
+  const [lastName, setLastName] = useState('')
+  const [showPopup, setShowPopup] = useState(false);
+  const [popupMessage, setPopupMessage] = useState('');
+
+  const handlePopup = (message) => {
+    setPopupMessage(message);
+    setShowPopup(true);
+  };
+
+  const handleClosePopup = () => {
+    setShowPopup(false);
+  };
 
   const handleSubmit = async (event) => {
     event.preventDefault();
-    try {
-      let res =await fetch("http://localhost:8080/customer/auth/register", {
-        method: "POST",
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-          firsrName: firstName,
-          lastName:lastName,
-          email: email,
-          password: password,
-        }),
-      });
-      let resJson = await res.json();
-      if (res.status === 200) {
-        setFirstName("");
-        setLastName("");
-        setEmail("");
-        setPassword("");
-       setMessage("Registration is succesful.")
-        console.log(setEmail)
-      } else {
-        setMessage("Some error occured");
-      }
-    } catch (err) {
-      //console.log(err);
+
+    let res = await fetch("http://localhost:8080/customer/auth/register", {
+      method: "POST",
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        firsrName: firstName,
+        lastName: lastName,
+        email: email,
+        password: password,
+      }),
+    });
+    let resJson = res;
+    if (res.status === 200) {
+      setFirstName("");
+      setLastName("");
+      setEmail("");
+      setPassword("");
+      setPopupMessage('success')
+    } else {
+      setPopupMessage('error')
+      console.log(resJson)
     }
+
+
   };
 
   return (
-    
-      <Row className="p-4 d-flex justify-content-center align-items-center">
-        <Col md={8} lg={6} xs={12}>         
-          <Card className="shadow">
+
+    <Row className="p-4 d-flex justify-content-center align-items-center">
+      <Col md={8} lg={6} xs={12}>
+        <Card className="shadow">
           <div className="square border border-3 border-primary">
-            <Card.Body>               
+            <Card.Body>
               <div className='mb-3 mt-md-4'>
                 <h1 className="fw-bold mb-2 text-center ">Registration</h1>
                 <p className='mb-3 text-center'>Please add your firstname,lastname,email and password!</p>
                 <div className='mb-3'>
                   <Form onSubmit={handleSubmit} className="form">
-                  <FormGroup controlId='firstname' className="mb-3">
+                    <FormGroup controlId='firstname' className="mb-3">
                       <FormLabel className='text-center' >First name:</FormLabel>
                       <FormControl
                         type="text"
                         value={firstName}
                         placeholder="First name"
-                        required="required" pattern="[A-Za-z0-9]{1,20}" autocomplete="off" 
+                        required="required" pattern="[A-Za-z0-9]{1,20}" autocomplete="off"
                         onChange={(event) => setFirstName(event.target.value)}
                       />
                     </FormGroup>
@@ -69,10 +82,10 @@ const RegistrationPage = () => {
                         type="Last name"
                         placeholder="Last name"
                         value={lastName}
-                        required="required" pattern="[A-Za-z0-9]{1,20}" autocomplete="off" 
+                        required="required" pattern="[A-Za-z0-9]{1,20}" autocomplete="on"
                         onChange={(event) => setLastName(event.target.value)}
                       />
-                      </FormGroup>
+                    </FormGroup>
 
                     <FormGroup controlId='email' className="mb-3">
                       <FormLabel className='text-center' >Email:</FormLabel>
@@ -80,7 +93,7 @@ const RegistrationPage = () => {
                         type="email"
                         value={email}
                         placeholder="Enter email"
-                        required="required" pattern="^[^\s@]+@[^\s@]+\.[^\s@]+$" autocomplete="on" 
+                        required="required" pattern="^[^\s@]+@[^\s@]+\.[^\s@]+$" autocomplete="on"
                         onChange={(event) => setEmail(event.target.value)}
                       />
                     </FormGroup>
@@ -90,21 +103,27 @@ const RegistrationPage = () => {
                         type="password"
                         placeholder="Password"
                         value={password}
-                        required="required" pattern="[A-Za-z0-9]{1,20}" autocomplete="off" 
+                        required="required" pattern="[A-Za-z0-9]{1,20}" autocomplete="off"
                         onChange={(event) => setPassword(event.target.value)}
                       />
 
-                    </FormGroup>                    
+                    </FormGroup>
                     <div className="d-grid">
-                    <Button variant="primary" type="submit">Register</Button>
+                      <Button variant="primary" type="submit" onClick={handlePopup}>Register</Button>
+                      {popupMessage === 'success' && (
+                        showPopup && <Popup message='The registartion is successful!' onClose={handleClosePopup} />
+                      )}
+                      {popupMessage === 'error' && (
+                        showPopup && <Popup message='The registartion is not successful!' onClose={handleClosePopup} />
+                      )}
                     </div>
-                  </Form>                 
-                  </div>
-                </div>             
+                  </Form>
+                </div>
+              </div>
             </Card.Body> </div>
-          </Card>
-        </Col>
-      </Row>
+        </Card>
+      </Col>
+    </Row>
   );
 };
 
