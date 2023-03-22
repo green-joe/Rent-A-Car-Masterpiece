e2f034fb49d7721f17685b5f7cc629e1ccf27ba9
sz-jozsef <sz.jozsef.developer@gmail.com>
2023-03-22 09:49:00 +0100

    logout endpoint and get customer by email endpoint is created

--- rentacarbackend/src/main/java/com/carrental/app/controllers/CustomerController.java
+++ rentacarbackend/src/main/java/com/carrental/app/controllers/CustomerController.java
@@ -2,7 +2,6 @@ package com.carrental.app.controllers;
 
 import com.carrental.app.CustomerExceptions.InvalidPasswordException;
 import com.carrental.app.models.Customer;
-import com.carrental.app.models.authresponse.AuthResponse;
 import com.carrental.app.repositories.CustomerRepository;
 import com.carrental.app.services.CustomerService;
 import lombok.RequiredArgsConstructor;
@@ -15,11 +14,18 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.management.InstanceNotFoundException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 @RestController
 @RequestMapping("/customer")
@@ -47,7 +53,7 @@ public class CustomerController {
         }
         try {
             customerService.isValidPassword(customer.getPassword());
-        }catch (InvalidPasswordException e){
+        } catch (InvalidPasswordException e) {
             return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
         }
         // create customer object
@@ -62,16 +68,34 @@ public class CustomerController {
     }
 
     @PostMapping("/auth/login")
-    public ResponseEntity<Object> authenticateUser(@RequestBody Customer customer) {
+    public ResponseEntity<String> authenticateUser(@RequestBody Customer customer) {
         try {
             Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(
-                  customer.getEmail(),customer.getPassword()));
+                    customer.getEmail(), customer.getPassword()));
             SecurityContextHolder.getContext().setAuthentication(authentication);
             String message = "User signed-in successfully!";
-            return new ResponseEntity<>(new AuthResponse(message,customer), HttpStatus.OK);
+            return new ResponseEntity<>(message, HttpStatus.OK);
         } catch (AuthenticationException e) {
             return new ResponseEntity<>("Authentication failed: " + e.getMessage(), HttpStatus.UNAUTHORIZED);
         }
     }
 
+    @GetMapping("/auth/logout")
+    public ResponseEntity<String> logoutUser(HttpServletRequest request, HttpServletResponse response) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication != null) {
+            new SecurityContextLogoutHandler().logout(request, response, authentication);
+        }
+        return new ResponseEntity<>("Logged out successfully", HttpStatus.OK);
+    }
+
+    @GetMapping("/get/customer")
+    public ResponseEntity<Customer> getCustomerByEmail(@RequestParam("email") String email) throws InstanceNotFoundException {
+        Customer customer = customerRepository.findByEmail(email)
+                .orElseThrow(() -> new InstanceNotFoundException("Customer not found for email: " + email));
+        return ResponseEntity.ok(customer);
+    }
 }
+
+
+
