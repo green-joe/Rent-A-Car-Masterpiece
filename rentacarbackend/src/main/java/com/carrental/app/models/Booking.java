24572ff02d374b16af30b9114b07cb4236244a31
green-joe <sz.jozsef.generali@gmail.com>
2023-04-24 16:41:11 +0200

    booking class is modified 2 properties

--- rentacarbackend/src/main/java/com/carrental/app/models/Booking.java
+++ rentacarbackend/src/main/java/com/carrental/app/models/Booking.java
@@ -16,6 +16,7 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import java.sql.Date;
+import java.sql.Time;
 
 @Entity
 @Table(name="booking")
@@ -41,6 +42,11 @@ public class Booking {
 
     @Column(name = "return_date")
     private Date returnDate;
+    @Column(name = "pickup_time")
+    private Time pickupTime;
+
+    @Column(name = "return_time")
+    private Time returnTime;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "car_image_id")
